e4c910481d159a2cfaa135f808991267aafa048a
Thibault Podechard <titi.podechard@gmail.com>
2024-12-12 12:11:15 +0100

    Ajoutez des fichiers projet.

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+//Nom du projet : TP IDE Et Premier Programme
+//Nom de l'auteur : Podechard Thibault 
+//Date de création : 10 Septembre 2024
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <math.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+int main();
+	{
+		int hex(nbHex);
+		printf("Entrer un nombre en hexadecimal : ");
+		scanf_s("%x", &nbHex);
+		printf("Vous avez saisi %#x (%d).\n", nbHex, nbHex);
+		_getch();
+		return(0)
+	}
